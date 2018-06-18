88bdd57c2862f6e922395b5c79ced846a3fcb114
Chasen Arey <ccarey@ncsu.edu>
2018-06-17 21:46:34 -0600

    final functionality for weekend

--- index.js
+++ index.js
@@ -2,13 +2,7 @@
 
 // `STORE` is responsible for storing the underlying data
 // that our app needs to keep track of in order to work.
-//
-// for a shopping list, our data model is pretty simple.
-// we just have an array of shopping list items. each one
-// is an object with a `name` and a `checked` property that
-// indicates if it's checked off or not.
-// we're pre-adding items to the shopping list so there's
-// something to see when the page first loads.
+
 const STORE = [
   {name: "apples", checked: false},
   {name: "oranges", checked: false},
@@ -16,6 +10,7 @@ const STORE = [
   {name: "bread", checked: false},
   {name: "butter", checked: true}
 ];
+
 
 function generateItemElement(item, itemIndex, template) {
   return `
@@ -63,12 +58,6 @@ function addItemToShoppingList(itemName) {
 
 function handleNewItemSubmit() {
   // this function will be responsible for when users add a new shopping list item
-  //Listen for when users submit a new list item. And then...
-  // Get the name of the new item from the text input in our new item form
-  // Clear out the value from the input so eventually new items can be added
-  // Create an object representing the new item and add it to the shopping list STORE
-  // Re-render the shopping list in the DOM in light of the updated STORE.
-
   $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
 
@@ -79,7 +68,7 @@ function handleNewItemSubmit() {
     renderShoppingList();
 
   });
-  // console.log('`handleNewItemSubmit` ran');
+  
   
 }
 
@@ -95,13 +84,9 @@ function getItemIndexFromElement(item) {
   return parseInt(itemIndexString, 10);
 }
 
+// this function will be responsible for when users click the "check" button 
 function handleItemCheckClicked() {
-  // this function will be responsible for when users click the "check" button on
-  // a shopping list item.
-  // Listen for when a user clicks the 'check' button on an item.
-  // Retrieve the item's index in STORE from the data attribute.
-  // Toggle the checked property for the item at that index in STORE.
-  // Re-render the shopping list.
+  
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
     console.log('`handleItemCheckClicked` ran');
     const itemIndex = getItemIndexFromElement(event.currentTarget);
@@ -110,10 +95,9 @@ function handleItemCheckClicked() {
   });
 }
 
-
-function handleDeleteItemClicked() {
-  // this function will be responsible for when users want to delete a shopping list
+// this function will be responsible for when users want to delete a shopping list
   // item
+function handleDeleteItemClicked() {
   $('.js-shopping-list').on('click', '.js-item-delete', event => {
     // alert('`handleDeleteItemClicked` ran');
     const itemIndex = getItemIndexFromElement(event.currentTarget);
@@ -127,17 +111,9 @@ function deleteItemFromShoppingList(itemIndex){
 }
 
 function handleUncheckedButtonClicked(){
-  //need to create a checkbox 
-  //need to toggle to display only unchecked items
-  // Retrieve the item's index in STORE from the data attribute.
-  // Get only checked false turn hide toggle on off.
-  // Re-render the shopping list.
-  
-  $('input[type=checkbox]').on('change', function(){
+    $('input[type=checkbox]').on('change', function(){
     // alert($(this).is(':checked'));
     const itemIndex = getItemIndexFromElement(event.currentTarget);
-    ///if checked true then toggle all hide
-    ///if clicked again toggle all back 
     if ($(this).is(':checked')) {
       // alert('check') and showing all webpage loads this way so flipped hide/show;
       $('.shopping-item__checked').closest('li').hide(600);
@@ -153,7 +129,7 @@ function handleUncheckedButtonClicked(){
 
 function handleItemSearch(){
   // $('#js-search-list-form').submit(function(){
-    $('#js-search-list-form').on( 'keyup' , function(){
+  $('#js-search-list-form').on( 'keyup' , function(){
     event.preventDefault();//prevent default form input processing
     let searchVal = $('.js-search-entry').val().toLowerCase();
     // console.log(searchVal);
@@ -164,36 +140,28 @@ function handleItemSearch(){
   });
 }
 
-function handleEditItemInput(){
-  // this function will be responsible for when users click the "edit" button on
+// this function will be responsible for when users click the "edit" button on
   // a shopping list item.
-  // Listen for when a user clicks the 'edit' button on an item.
-  //make an area for the item to be edited
-  // Retrieve the item's index in STORE from the data attribute.
-  // edit the items name property for the item at that index in STORE.
-  // Re-render the shopping list.
+function handleEditItemInput(){
   $('.js-shopping-list').on('click', '.js-item-edit', event => {
     // console.log('`handleEditItemClicked` ran');
     $(event.currentTarget).closest('li').find('.item-edit-box').show();
     $(event.currentTarget).closest('li').find('.item-edit-submit').show();
     // console.log(event.currentTarget); 
-    // const itemIndex = getItemIndexFromElement(event.currentTarget);
-    // toggleCheckedForListItem(itemIndex);
-    // renderShoppingList()
+    // renderShoppingList();
      });
 }
 
 function handleEditItemSubmit(){
-  
-  $('.js-shopping-list').on('click', '.item-edit-submit', function(event) {  
-    const editedItemName = $('.item-edit-box').val();
+  $('.js-shopping-list').on('click', '.item-edit-submit', function(event) { 
+    console.log('lllllll'); 
+    let editedItemName = $(event.currentTarget).closest('.js-shopping-item').find('.item-edit-box').val();
+    console.log(editedItemName);
     const itemIndex = getItemIndexFromElement(event.currentTarget);
 
     if(editedItemName !== ''){
       STORE[itemIndex].name = editedItemName;
-    }else{
-      // $('js.item.edit').
-    };
+    }
 
     renderShoppingList();
   });
